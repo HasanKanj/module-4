a48bebb4ca3d7091390264ef2e678c57815f5608
HasanKanj <73579930+HasanKanj@users.noreply.github.com>
2020-12-14 01:54:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function () {
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Yaakov", "Jackob", "John", "Jade", "Peter", "Faris", "laurin", "Paul", "Layla", "Jane"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
